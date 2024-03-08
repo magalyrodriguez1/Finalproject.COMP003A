f649e64ed5b4d9371aff6044057e8e600ee3ab46
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-03-07 21:08:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Finalproject.COMP3A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name.");
+            string name = Console.ReadLine();
+            if (name is not string)
+            {
+
+
+                Console.WriteLine("Enter your last name.");
+                string lastName = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Name is not valid. Enter letters only.");
+            }
+
+            Console.WriteLine("Enter your birth year.");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Birthyear(year);
+
+            
+
+
+
+        }
+        /*
+        static void Name(string name)
+        {
+            if (name is string)
+            {
+                //string lastName = (Console.ReadLine);
+                Console.WriteLine($"{lastName}, {name}");
+
+            }
+            else
+            {
+                Console.WriteLine("Name is not valid. Enter letters only.");
+            }
+        */
+
+        static void Birthyear(int year)
+        {           
+
+            if (year >= 1900 && year <= 2024)
+            {
+               year = DateTime.Now.Year - year;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid birth year.");
+            }
+             Console.WriteLine($"{year}");
+        }
+    }
+}
+
